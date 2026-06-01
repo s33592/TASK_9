fe6fc04d0830664a4406f7c9516d620a4fa845f1
Krasimir Ronkov <s33592@pjwstk.edu.pl>
2026-06-01 22:29:19 +0200

    The classes representing the models - implemented

---
+++ Models/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TASK_9.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        public string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Models/UserNote.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TASK_9.Models
+{
+    public class UserNote
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int AppUserId { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("AppUserId")]
+        public AppUser? AppUser { get; set; }
+
+
+    }
+}
